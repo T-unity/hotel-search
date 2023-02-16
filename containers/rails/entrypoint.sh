19af40e5862330249169962e7b82415ef63c0a01
T-unity <takadanaritoshi@gmail.com>
2023-02-17 02:19:03 +0900

    Add comment

--- containers/rails/entrypoint.sh
+++ containers/rails/entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 
 rm -f /myapp/tmp/pids/server.pid
 
+# このコマンドは何度も実行されると困る（本番アプリケーションの初回起動時のみ実行したい）ため、何か対応方法を検討する。
 bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rails db:seed
